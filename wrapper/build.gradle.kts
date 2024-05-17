1578f4be1dd4d662481827ba1bb86dedebcd0738
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2024-05-17 19:16:50 +0200

    added dependencies to Wrapper

--- wrapper/build.gradle.kts
+++ wrapper/build.gradle.kts
@@ -1,16 +1,19 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    kotlin("plugin.serialization") version "1.9.23"
 }
 
 group = "me.blueamethyst.bluecloud"
-version = "unspecified"
+version = project.property("project.version").toString()
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation(project(":common"))
+    implementation(project(":runner"))
+    implementation(libs.kotlinxSerializationCore)
 }
 
 tasks.test {
