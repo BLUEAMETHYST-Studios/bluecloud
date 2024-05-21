e466f5435ef2f23a2e5e846162b285dcb7d447e2
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2024-05-21 13:10:21 +0200

    Update ConsoleTest.kt

--- common/src/test/kotlin/ConsoleTest.kt
+++ common/src/test/kotlin/ConsoleTest.kt
@@ -1,3 +1,4 @@
+import com.github.ajalt.clikt.core.CliktCommand
 import me.blueamethyst.bluecloud.common.console.ConsoleColors
 import me.blueamethyst.bluecloud.common.console.Logger
 import me.blueamethyst.bluecloud.common.console.cli.Terminal
@@ -7,12 +8,21 @@ fun main() {
     val terminal = Terminal(
         Logger("Terminal", true),
         "${ConsoleColors.YELLOW_BRIGHT}CLI${ConsoleColors.BLACK_BRIGHT}@${ConsoleColors.BLUE_BRIGHT}BlueCloud ${ConsoleColors.BLACK_BRIGHT}» ${ConsoleColors.RESET}",
-        listOf()
+        listOf(
+            MySubcommand()
+        )
     )
 
     terminal.start()
 
     while (true) {
         Thread.sleep(1000)
+    }
+}
+
+class MySubcommand: CliktCommand(name = "subcommand") {
+
+    override fun run() {
+        println("Hello from subcommand!")
     }
 }
